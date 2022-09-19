78cafdecc86a218710589bf3f160fe7a42971bf9
AyalaKialy <ayalak4207@gmail.com>
2022-09-19 23:33:52 +0300

    add tinyUrl functionality

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import routes from './routes/shortUrl.routes.js';
 import db from './db.js';
 import dotenv from 'dotenv';
-import crypto from 'crypto';
 
 dotenv.config();
 const app = express();
@@ -10,18 +9,6 @@ const port = process.env.PORT || 3333;
 
 app.use(express.json());
 
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//     const hash = crypto.createHash('sha256');
-//     const finalHex = hash.update(JSON.stringify(req.body)).digest('hex');
-//     res.locals.hash = { "SHA256": finalHex };
-//     next();
-// })
-
-// app.use('/', (req: Request, res: Response, next: NextFunction) => {
-
-//     res.send(res.locals.hash.SHA256);
-
-// })
 app.listen(port, () => {
     console.log(`Application listening at http://localhost:${port}`);
     db();
